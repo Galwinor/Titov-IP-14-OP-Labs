1f8c1c876c43c65b96f87969502da27fa382a03a
Galwinor <89968150+Galwinor@users.noreply.github.com>
2022-03-25 12:42:00 +0200

    Delete Laba2 directory

--- Laba2/OP2.2Python/autoBase.py
+++
@@ -1,6 +0,0 @@
-from date import Date
-class AutoBase:
-    def __init__(self, Name, Data1, Data2):
-        self.Name = Name
-        self.DateR = Data1
-        self.DateSR = Data2
--- Laba2/OP2.2Python/date.py
+++
@@ -1,5 +0,0 @@
-class Date:
-    def __init__(self, day, mounth, year):
-        self.day = day
-        self.mounth = mounth
-        self.year = year
